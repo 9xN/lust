ef4e2c75178459c198101664c003cfc0d5e6c796
social reject <94141577+9xN@users.noreply.github.com>
2022-04-21 14:19:50 -0600

    add better colours + format & fix port forwarding

--- cli.js
+++ cli.js
@@ -1,17 +1,14 @@
 const args = require('args')
-const ngrok = require('ngrok');
-const fetch = require('node-fetch')
-const config = require('./config.json')
-const authtoken = config.authtoken
 require('colors')
 
 const { start } = require('./lib/index')
 
 args
-    .option(['h', 'master-host'], 'Master host to bind the listening server to', '127.0.0.1')
-    .option(['p', 'master-port'], 'The port on which the server will be listening for masters', 4445)
-    .option(['H', 'client-host'], 'Clients host to bind the listening server to', '0.0.0.0')
-    .option(['P', 'client-port'], 'The port on which the server will be listening for clients', 4444)
+    .option(['h', '--master-host'], 'Master host to bind the listening server to', '127.0.0.1')
+    .option(['p', '--master-port'], 'The port on which the server will be listening for masters', 4445)
+    .option(['H', '--client-host'], 'Clients host to bind the listening server to', '0.0.0.0')
+    .option(['P', '--client-port'], 'The port on which the server will be listening for clients', 4444)
+    .option(['a', '--ngrok-api-key'], 'Your ngrok api key used for port forwarding')
 
 const flags = args.parse(process.argv)
 
@@ -19,34 +16,6 @@ start({
     MASTER_HOST: flags.masterHost,
     MASTER_PORT: flags.masterPort,
     CLIENT_HOST: flags.clientHost,
-    CLIENT_PORT: flags.clientPort
+    CLIENT_PORT: flags.clientPort,
+    NGROK_TOKEN: flags.ngrokApiKey
 })
-
-
-
-
-async function main() {
-    for (let port = 4444; port < 4446; port++) {
-        console.log(`Forwarding port ${port} to:`.green)
-        await ngrok.connect({
-            proto: 'tcp',
-            addr: port,
-            authtoken: authtoken,
-            region: 'us'
-        });
-        await fetch('http://localhost:4040/api/tunnels')
-            .then(res => res.json())
-            .then(json => json.tunnels.map(tunnel => tunnel.public_url))
-            .then(publicUrls => console.log(publicUrls[0].blue)) //, publicUrls.forEach(url => console.log(url)))
-            .catch(err => {
-                if (err.code === 'ECONNREFUSED') {
-                    return console.error(
-                        "Looks like you're not running ngrok."
-                    )
-                }
-                return console.error(err)
-            })
-    }
-}
-
-main();
